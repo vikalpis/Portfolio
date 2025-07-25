a80b10e54187d7cdee1408789694cd2c9d27f831
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-07-25 20:39:58 +0530

    tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div >
-        <div style={{display : "flex", background : "black", height: "10vh", width : "100%", borderRadius :"20px", margin:"1vh",justifyContent :"center", alignItems:"center", color:"white", gap:"2rem"}}>
+        <div className='flex text-'>
          
                 <p>Home</p>
                 <p>Skills</p>
