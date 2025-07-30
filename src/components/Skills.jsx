984da955bdffc392efa0211ad4ec920955d6bd51
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-07-30 20:22:24 +0530

    .

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ export default function Skills(){
         <>
         <div className="font-bold text-3xl text-white bg-transparent leading-loose px-2 ">Skills</div>
         
-        <div className="gap-6 bg-transparent ">
+        <div className="gap-6 bg-transparent p-2">
        
         
         <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
