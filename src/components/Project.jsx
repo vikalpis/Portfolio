d7a241096de4f0f7bc144285cd6fde3ea3b290dc
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-07-30 10:04:43 +0530

    updated 1.1.2

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -15,32 +15,31 @@ export default function Project(){
         <div className={`grid grid-cols-1 sm:grid-cols-2 gap-4 text-white bg-transparent px-2 `}>
 
  {/*1st */}           
-  <div className={`transition-all duration-500 hover:scale-105 cursor-pointer w-full bg-black border rounded-3xl ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
+  <div className={`transition-all duration-500 hover:scale-105 cursor-pointer w-full bg-black border rounded-3xl ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"} hover:bg-gradient-to-br from-white/55 to-black `}>
     <p className="font-bold text-lg p-4 pt-8 ">
       Project Name
     </p>
     
-    <p className="p-4 leading-tight text-justify">
+    <p className="p-4 leading-tight text-justify text-white/85">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, eaque sapiente voluptas ullam officia amet nesciunt laboriosam vol
     </p>
 
-    {/* Button container with spacing and responsive left margin */}
     <div className="flex gap-4 ml-4 sm:ml-4 md:ml-8 mb-2.5 ">
       <button className="transition-all duration-500 hover:scale-x-125 rounded-lg p-2 bg-transparent"><Link/></button>
       <button className="transition-all duration-500 hover:scale-x-125 rounded-lg p-2 bg-transparent"><MoveUpRight/></button>
     </div>
   </div>
  {/*2nd */}
- <div className={`transition-all duration-500 hover:scale-105 cursor-pointer w-full bg-black border rounded-3xl ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
+ <div className={`transition-all duration-500 hover:scale-105 cursor-pointer w-full bg-black border rounded-3xl ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"} hover:bg-gradient-to-br from-white/55 to-black`}>
  <p className="font-bold text-lg p-4 pt-8 ">
       Project Name
     </p>
     
-    <p className="p-4 leading-tight text-justify">
+    <p className="p-4 leading-tight text-justify text-white/85">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, eaque sapiente voluptas ullam officia amet nesciunt laboriosam vol
     </p>
 
-    {/* Button container with spacing and responsive left margin */}
+    
     <div className="flex gap-4 ml-4 sm:ml-4 md:ml-8 mb-2.5">
     <button className="transition-all duration-500 hover:scale-x-125 rounded-lg p-2 bg-transparent"><Link/></button>
     <button className="transition-all duration-500 hover:scale-x-125 rounded-lg p-2 bg-transparent"><MoveUpRight/></button>
@@ -48,16 +47,15 @@ export default function Project(){
   </div>
 
 {/*3rd */}
-<div className={`transition-all duration-500 hover:scale-105 cursor-pointer w-full bg-black border rounded-3xl ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
+<div className={`transition-all duration-500 hover:scale-105 cursor-pointer w-full bg-black border rounded-3xl ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"} hover:bg-gradient-to-br from-white/55 to-black`}>
   <p className="font-bold text-lg p-4 pt-8 ">
       Project Name
     </p>
     
-    <p className="p-4 leading-tight text-justify">
+    <p className="p-4 leading-tight text-justify text-white/85">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, eaque sapiente voluptas ullam officia amet nesciunt laboriosam vol
     </p>
 
-    {/* Button container with spacing and responsive left margin */}
     <div className="flex gap-4 ml-4 sm:ml-4 md:ml-8 mb-2.5">
     <button className="transition-all duration-500 hover:scale-x-125 rounded-lg p-2 bg-transparent"><Link/></button>
     <button className="transition-all duration-500 hover:scale-x-125 rounded-lg p-2 bg-transparent"><MoveUpRight/></button>
@@ -65,12 +63,12 @@ export default function Project(){
   </div>
 
 {/*4th */}
-<div className={`transition-all duration-500 hover:scale-105 cursor-pointer w-full bg-black border rounded-3xl ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
+<div className={`transition-all duration-500 hover:scale-105 cursor-pointer w-full bg-black border rounded-3xl ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"} hover:bg-gradient-to-br from-white/55 to-black`}>
   <p className="font-bold text-lg p-4 pt-8 ">
       Project Name
     </p>
     
-    <p className="p-4 leading-tight text-justify">
+    <p className="p-4 leading-tight text-justify text-white/85">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, eaque sapiente voluptas ullam officia amet nesciunt laboriosam vol
     </p>
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,79 +9,79 @@ export default function Skills(){
         <div className="gap-6 bg-transparent ">
        
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         MongoDB
         </span>
         </button>
 
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         Express.js
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         React.js
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         Node.js
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         TailwindCSS
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         HTML
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         CSS
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         JavaScipt
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         TypeScript
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         Recoil
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         Git/Github
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         REST API
         </span>
         </button>
         
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white to-black   dark:text-white ">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-white/55 to-black   dark:text-white ">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent">
         C++
         </span>
